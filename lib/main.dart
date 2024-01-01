fba8cbc792ca0daed50207a2e6d2a905f0dd4484
Adam3131 <90896452+Adam3131@users.noreply.github.com>
2024-01-01 23:29:12 +0700

    Refactor conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:capstone_project/pages/lost_item.dart';
-//import 'package:capstone_project/pages/login_page.dart';
+import 'package:capstone_project/pages/login_page.dart';
 //import 'package:capstone_project/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
