0f4b521db8db73f61a033f5a9db4c303f27ad4a9
Adam3131 <90896452+Adam3131@users.noreply.github.com>
2024-02-07 09:54:41 +0700

    Found Item List Page (#40)

---
+++ lib/components/found_item_list_card.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class FoundItemListCard extends StatelessWidget {
+ const FoundItemListCard({super.key});
+
+ @override
+ Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          child: Column(
+            children: [
+              ListTile(
+               leading: const Icon(Icons.album, size: 35,),
+               title: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   Text('ADAM'),
+                   Text('23/12/23')
+                 ],
+               ),
+               subtitle: Card(
+                 elevation: 0,
+                 color: Theme.of(context).colorScheme.surfaceVariant,
+                 child: const SizedBox(
+                    height: 70,
+                    child: Center(child: Text('Description Text')),
+                 ),
+               ),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                 Padding(
+                   padding: EdgeInsets.only(left: 64),
+                   child: Expanded(
+                      child: Row(
+                        children: [
+                          Icon(Icons.location_on, color: Color.fromRGBO(43, 52, 153, 1),),
+                          Text('Cikarang Station'),
+                        ],
+                      ),
+                   ),
+                 ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+ }
+}
