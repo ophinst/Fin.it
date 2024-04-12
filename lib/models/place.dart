ebf6d16aa290cbcc6c51738b24a93623f7dd5ba5
Adam3131 <90896452+Adam3131@users.noreply.github.com>
2024-04-13 00:16:47 +0700

    [ Post found and lost form ] - Implement get current location - Implement pinpoint location - Implement native device features like camera and maps - Successfully post for the found and lost form to the cloud - Upgrade AGP and Kotlin version

---
+++ lib/models/place.dart
@@ -0,0 +1,19 @@
+class PlaceLocation {
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.locationDetail,
+  });
+  
+  final String latitude;
+  final String longitude;
+  final String locationDetail;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'latitude': latitude,
+      'longitude': longitude,
+      'locationDetail': locationDetail,
+    };
+  }
+}
