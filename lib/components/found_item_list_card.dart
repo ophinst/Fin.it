a576b974aba35c0ed69f13e14af244e79ccbc2c8
Adam3131 <90896452+Adam3131@users.noreply.github.com>
2024-04-23 21:44:12 +0700

    Get found item data from found item api (#62)
    * [ Post found and lost form ]
    - Implement get current location
    - Implement pinpoint location
    - Implement native device features like camera and maps
    - Successfully post for the found and lost form to the cloud
    - Upgrade AGP and Kotlin version
    
    * [ Read env file for sensitive information ]
    
    - Successfully read .env
    - Check map features for post data into cloud
    
    * [ Get Found Item ]
    - Create model for get item api
    - get found item data
    - Adjust the View with the data fetched from the found item api

--- lib/components/found_item_list_card.dart
+++ lib/components/found_item_list_card.dart
@@ -1,10 +1,12 @@
+import 'package:capstone_project/models/found_model.dart';
 import 'package:flutter/material.dart';
 
 class FoundItemListCard extends StatelessWidget {
- const FoundItemListCard({super.key});
+  final GetFoundModel foundItem;
+  const FoundItemListCard({required this.foundItem, super.key});
 
- @override
- Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {},
       child: Padding(
@@ -13,37 +15,43 @@ class FoundItemListCard extends StatelessWidget {
           child: Column(
             children: [
               ListTile(
-               leading: const Icon(Icons.album, size: 35,),
-               title: const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: [
-                   Text('ADAM'),
-                   Text('23/12/23')
-                 ],
-               ),
-               subtitle: Card(
-                 elevation: 0,
-                 color: Theme.of(context).colorScheme.surfaceVariant,
-                 child: const SizedBox(
+                leading: const Icon(
+                  Icons.album,
+                  size: 35,
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(foundItem.foundOwner),
+                    Text(foundItem.foundDate),
+                  ],
+                ),
+                subtitle: Card(
+                  elevation: 0,
+                  color: Theme.of(context).colorScheme.surfaceVariant,
+                  child: SizedBox(
                     height: 70,
-                    child: Center(child: Text('Description Text')),
-                 ),
-               ),
+                    child: Center(child: Text(foundItem.itemDescription)),
+                  ),
+                ),
               ),
-              const Row(
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                 Padding(
-                   padding: EdgeInsets.only(left: 64),
-                   child: Expanded(
+                  Padding(
+                    padding: const EdgeInsets.only(left: 64),
+                    child: Expanded(
                       child: Row(
                         children: [
-                          Icon(Icons.location_on, color: Color.fromRGBO(43, 52, 153, 1),),
-                          Text('Cikarang Station'),
+                          const Icon(
+                            Icons.location_on,
+                            color: Color.fromRGBO(43, 52, 153, 1),
+                          ),
+                          Text(foundItem.placeLocation.locationDetail),
                         ],
                       ),
-                   ),
-                 ),
+                    ),
+                  ),
                 ],
               ),
             ],
@@ -51,5 +59,5 @@ class FoundItemListCard extends StatelessWidget {
         ),
       ),
     );
- }
+  }
 }
