07d14b4c86567e068b9fd89b21b83f001065a736
Adam3131 <90896452+Adam3131@users.noreply.github.com>
2024-04-23 21:34:15 +0700

    [ Get Found Item ] - Create model for get item api - get found item data - Adjust the View with the data fetched from the found item api

--- lib/models/found_model.dart
+++ lib/models/found_model.dart
@@ -28,3 +28,45 @@ class FoundModel {
     };
   }
 }
+
+class GetFoundModel {
+  final String foundId;
+  final String uid;
+  final String itemName;
+  final String itemDescription;
+  final String foundDate;
+  final String foundTime;
+  final String category;
+  final PlaceLocation placeLocation;
+  final String foundOwner;
+
+  GetFoundModel({
+    required this.foundId,
+    required this.uid,
+    required this.itemName,
+    required this.itemDescription,
+    required this.foundDate,
+    required this.foundTime,
+    required this.category,
+    required this.placeLocation,
+    required this.foundOwner,
+  });
+
+  factory GetFoundModel.fromJson(Map<String, dynamic> json) {
+    return GetFoundModel(
+      foundId: json['foundId'],
+      uid: json['uid'],
+      itemName: json['itemName'],
+      itemDescription: json['itemDescription'],
+      foundDate: json['foundDate'],
+      foundTime: json['foundTime'],
+      category: json['category'],
+      placeLocation: PlaceLocation(
+        latitude: json['latitude'],
+        longitude: json['longitude'],
+        locationDetail: json['locationDetail'],
+      ),
+      foundOwner: json['foundOwner'],
+    );
+  }
+}
